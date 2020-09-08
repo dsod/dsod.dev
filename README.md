5214e811bc38cdbef5c2cab228cd5d16fb1972c3
dsod <d.soderling@live.se>
2020-09-08 18:28:02 +0200

    Set up gatsby project with typescript and cloudflare worker deployment

---
+++ README.md
@@ -0,0 +1,9 @@
+# My portfolio site
+
+## Tech stack
+
+I have used the following technologies when building the site:
+
+- Gatsby Static Site Generator
+- React with Typescript
+- SCSS
---
+++ gatsby-ssr.js
@@ -0,0 +1,19 @@
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
+
+// You can delete this file if you're not using it
+import * as React from "react"
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  const NoFollow = (
+    <meta
+      name="robots"
+      content="noindex, nofollow, noimageindex"
+      data-no-index="true"
+    />
+  )
+  setHeadComponents([NoFollow])
+}
