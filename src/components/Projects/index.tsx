dbce923f80f97519311f73242b796a97b75c6656
dsod <d.soderling@live.se>
2020-09-23 17:40:43 +0200

    Fix for the timeline on small devides

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -4,7 +4,7 @@ import SectionHeader from '../Shared/SectionHeader';
 const Projects = () => (
     <section id='projects' className='container'>
         <div className='row justify-content-center'>
-            <div className='col-6 col-md-5 col-lg-4'>
+            <div className='col-7 col-md-6 col-lg-5'>
                 <SectionHeader section='Projects' />
             </div>
         </div>
